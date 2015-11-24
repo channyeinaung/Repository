a2a0fca7a68125476eff40a73d0816192c1283d3
channyeinaung <channyeinaung87@gmail.com>
2015-11-24 10:21:55 +0800

    Demo
    My Demo file

---
+++ info.php
@@ -0,0 +1,27 @@
+<?php
+	echo "Your IP is " .get_client_ip();
+	echo "<br/>Port: ".$_SERVER['SERVER_PORT'];
+	echo "<br/>HTTPS: ".$_SERVER['HTTPS'];
+	
+	phpinfo();
+	
+	function get_client_ip() {
+		$ipaddress = '';
+		if (getenv('HTTP_CLIENT_IP'))
+			$ipaddress = getenv('HTTP_CLIENT_IP');
+		else if(getenv('HTTP_X_FORWARDED_FOR'))
+			$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+		else if(getenv('HTTP_X_FORWARDED'))
+			$ipaddress = getenv('HTTP_X_FORWARDED');
+		else if(getenv('HTTP_FORWARDED_FOR'))
+			$ipaddress = getenv('HTTP_FORWARDED_FOR');
+		else if(getenv('HTTP_FORWARDED'))
+			$ipaddress = getenv('HTTP_FORWARDED');
+		else if(getenv('REMOTE_ADDR'))
+			$ipaddress = getenv('REMOTE_ADDR');
+		else
+			$ipaddress = 'UNKNOWN';
+	 
+		return $ipaddress;
+	}
+?>
